ce964e43ea87d40e640dda42d8d9c8c1f8f37b69
Martin Zink <martinzink@apache.org>
2023-12-03 16:10:50 +0100

    day3 init?

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,81 @@
+use std::cmp::max;
+use std::collections::HashMap;
+
+const EXAMPLE: &str = include_str!("example.txt");
+const INPUT: &str = include_str!("input.txt");
+
+#[derive(Debug)]
+struct Ball {
+    quantity: u32,
+    ball_color: String,
+}
+
+impl Ball {
+    fn new(input_str: &str) -> Self {
+        let trimmed=input_str.trim().split_ascii_whitespace().collect::<Vec<&str>>();
+        let quantity: u32 = trimmed.get(0).unwrap().parse::<u32>().unwrap();
+        let ball_color =  trimmed.get(1).unwrap().to_string();
+        Self{quantity, ball_color}
+    }
+}
+
+fn star_one(input_str: &str) -> u32 {
+    let max_balls: HashMap<&str, u32> = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
+
+    let mut sum = 0;
+
+    for input_line in input_str.lines() {
+        let split_line= input_line.split(':').collect::<Vec<&str>>();
+        let game_id = split_line.get(0).unwrap().split_ascii_whitespace().collect::<Vec<&str>>().get(1).unwrap().parse::<u32>().unwrap();
+        let mut game_data = split_line.get(1).unwrap().to_string();
+        let mut game_sets = game_data.split(';').collect::<Vec<&str>>();
+        let mut invalid: bool = false;
+        for game_set in game_sets {
+            let mut game_balls: HashMap<&str, u32> = HashMap::new();
+            for ball_str in game_set.split(',') {
+                let ball = Ball::new(ball_str);
+                invalid = invalid || max_balls.get(&*ball.ball_color).unwrap() < &ball.quantity;
+            }
+        }
+        if !invalid {
+            sum += game_id;
+        }
+
+    }
+    return sum;
+}
+
+fn star_two(input_str: &str) -> u32 {
+    let mut sum = 0;
+
+    for input_line in input_str.lines() {
+        let split_line= input_line.split(':').collect::<Vec<&str>>();
+        let mut game_data = split_line.get(1).unwrap().to_string();
+        let mut game_sets = game_data.split(';').collect::<Vec<&str>>();
+        let mut game_balls: HashMap<String, u32> = HashMap::from([
+            ("red".to_string(), 0),
+            ("green".to_string(), 0),
+            ("blue".to_string(), 0),
+        ]);
+
+        for game_set in game_sets {
+            for ball_str in game_set.split(',') {
+                let ball_d = Ball::new(ball_str);
+                let current_max = game_balls[&ball_d.ball_color as &str];
+                let current = ball_d.quantity;
+                game_balls.insert(ball_d.ball_color, max(current, current_max));
+            }
+        }
+        sum += game_balls.get("red").unwrap() * game_balls.get("green").unwrap() * game_balls.get("blue").unwrap();
+    }
+    return sum;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Example: {}", star_two(EXAMPLE));
+    println!("Input: {}", star_two(INPUT));
 }
---
+++ day3/src/main.rs
@@ -0,0 +1,118 @@
+use std::collections::{HashSet};
+use regex::Regex;
+
+const EXAMPLE: &str = include_str!("example.txt");
+const INPUT: &str = include_str!("input.txt");
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+impl Coord {
+    fn get_neighbours(&self) -> HashSet<Coord> {
+        let mut result = HashSet::new();
+        for i in -1..2 {
+            for j in -1..2 {
+                if i != 0 || j != 0 {
+                    result.insert(Coord{x:self.x+i, y:self.y+j});
+                }
+            }
+        }
+        return result;
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct EnginePart {
+    value: u32,
+    coords: Vec<Coord>,
+}
+
+
+fn extract_engines(input: &str, y:i32) -> Vec<EnginePart>{
+    let re = Regex::new(r"\d+").unwrap();
+
+    let matches: Vec<(usize, usize, &str)> = re
+        .find_iter(input)
+        .map(|mat| (mat.start(), mat.end(), mat.as_str()))
+        .collect();
+
+    let mut engine_parts = Vec::new();
+    for (start, end, number) in matches {
+        let mut coords: Vec<Coord> = Vec::new();
+        for x in start..end {
+            coords.push(Coord{x:x as i32, y});
+        }
+        engine_parts.push(EnginePart{value:number.parse::<u32>().unwrap(), coords});
+    }
+    return engine_parts;
+}
+
+fn star_two(input_str: &str) -> u32 {
+    let mut sum = 0;
+    let mut line_num:i32 = 0;
+    let mut gear_coords = HashSet::new();
+    let mut engine_parts: Vec<EnginePart> = Vec::new();
+    for input_line in input_str.lines() {
+        engine_parts.append(&mut extract_engines(input_line, line_num));
+        let mut char_num:i32 = 0;
+        for input_char in input_line.chars() {
+            if input_char == '*' {
+                gear_coords.insert(Coord{x:char_num, y:line_num});
+            }
+            char_num += 1;
+        }
+        line_num += 1;
+    }
+
+    for gear_coord in &gear_coords {
+        let mut neighbours = gear_coord.get_neighbours();
+        let engine_parts_next_to_gear =  engine_parts.iter().filter(|engine_part| { engine_part.coords.iter().any(|engine_coord| {neighbours.contains(engine_coord)}) }).cloned().collect::<Vec<EnginePart>>();
+        if engine_parts_next_to_gear.len() == 2 {
+            sum += (engine_parts_next_to_gear[0].value * engine_parts_next_to_gear[1].value);
+        }
+    }
+
+    return sum;
+}
+
+
+fn star_one(input_str: &str) -> u32 {
+    let mut sum = 0;
+    let mut line_num:i32 = 0;
+    let mut symbol_coords = HashSet::new();
+    let mut engine_parts: Vec<EnginePart> = Vec::new();
+    for input_line in input_str.lines() {
+        engine_parts.append(&mut extract_engines(input_line, line_num));
+        let mut char_num:i32 = 0;
+        for input_char in input_line.chars() {
+            if !input_char.is_numeric() && input_char != '.' {
+                symbol_coords.insert(Coord{x:char_num, y:line_num});
+            }
+            char_num += 1;
+        }
+        line_num += 1;
+    }
+
+    for engine_part in &engine_parts {
+        let mut neighbours = HashSet::new();
+        for coord in &engine_part.coords {
+            neighbours.extend(coord.get_neighbours());
+        }
+        let a: HashSet<Coord> = neighbours.intersection(&symbol_coords).cloned().collect();
+        if a.len() > 0 {
+            sum += engine_part.value;
+        }
+        println!("for engine {:?} neighbours are {:?}", engine_part, neighbours);
+    }
+    return sum;
+}
+
+
+
+fn main() {
+    println!("Example: {}", star_two(EXAMPLE));
+    println!("Input: {}", star_two(INPUT));
+}
