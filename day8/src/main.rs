fcadbe0fc5d7a976d55251ada512810654019101
Martin Zink <martinzink@apache.org>
2023-12-08 09:53:32 +0100

    day8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -55,22 +55,38 @@ fn star_one(input: &str) -> u32 {
     return steps;
 }
 
+fn gcd(a:  i64, b:  i64) ->  i64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a:  i64, b:  i64) ->  i64 {
+    if a == 0 || b == 0 {
+        0
+    } else {
+        a * b / gcd(a, b)
+    }
+}
+
 fn done(curr_nodes: &Vec<String>) -> bool {
     return curr_nodes.iter().all(|s|{s.ends_with('Z')});
 }
 
-fn star_two(input: &str) -> u32 {
+fn star_two(input: &str) -> i64 {
     let mut lines = input.lines();
     let instructions = lines.next().unwrap();
     let mut map = HashMap::new();
-    let mut curr_nodes = Vec::new();
+    let mut start_nodes = Vec::new();
     for line in lines {
         if line.is_empty() {
             continue;
         }
         if let Some((node_id, node)) = parse_line(line) {
             if node_id.ends_with('A') {
-                curr_nodes.push(node_id.clone());
+                start_nodes.push(node_id.clone());
             }
             map.insert(node_id, node);
         } else {
@@ -79,29 +95,47 @@ fn star_two(input: &str) -> u32 {
     }
     let mut steps = 0;
 
-    let mut instructions = instructions.chars().cycle();
-    while !done(&curr_nodes) {
-        if steps%1000 == 0 {
-            println!("{}", steps);
-        }
-        match instructions.next().unwrap() {
-            'L' => {
-                for curr_node in &mut curr_nodes {
-                    let curr_string_clone = curr_node.clone();
-                    curr_node.replace_range(.., &mut map.get(&curr_string_clone).unwrap().left.clone());
-                }
+    let mut good_coords_map = HashMap::new();
+    for start_node in start_nodes {
+        let mut coords = vec!(start_node.clone());
+
+        let mut good_coords = Vec::new();
+        let mut steps = 0;
+        let mut is_loop = false;
+        let instruction_length = instructions.len() as  i64;
+        let mut instructions = instructions.chars().cycle();
+        while !is_loop {
+            match instructions.next().unwrap() {
+                'L' => { coords.push(map.get(&coords.last().unwrap().clone()).unwrap().left.clone()) }
+                'R' => { coords.push( map.get(&coords.last().unwrap().clone()).unwrap().right.clone())}
+                _ => {panic!("Invalid instruction")}
             }
-            'R' => {
-                for curr_node in &mut curr_nodes {
-                    let curr_string_clone = curr_node.clone();
-                    curr_node.replace_range(.., &mut map.get(&curr_string_clone).unwrap().right.clone());
-                }
+            if coords.last().unwrap().ends_with('Z') {
+                good_coords.push(steps+1);
             }
-            _ => {panic!("Invalid instruction")}
+
+            let mut last_cycle = (coords.len() as  i64)-1-instruction_length;
+            while last_cycle > 0 {
+                let last_cycle_coord = coords.get(last_cycle as usize).unwrap();
+                let curr_coord = coords.last().unwrap();
+                if *last_cycle_coord == *curr_coord {
+                    is_loop = true;
+                    good_coords.retain(|&x| x >= last_cycle);
+                }
+                last_cycle = last_cycle-instruction_length;
+            }
+            steps += 1;
         }
-        steps = steps+1;
+        good_coords_map.insert(start_node, good_coords);
     }
-    return steps;
+    println!("{:?}", good_coords_map);
+    let mut values_to_fold = Vec::new();
+    for (_,good_coords) in good_coords_map {
+        values_to_fold.push(good_coords[0]);
+    }
+    let result = values_to_fold.iter().cloned().fold(1, lcm);
+
+    return result;
 }
 
 
