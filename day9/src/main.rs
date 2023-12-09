89592b1022367f824c26f920d3c30dda0131fd65
Martin Zink <martinzink@apache.org>
2023-12-09 10:02:53 +0100

    day9 star2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,6 +2,7 @@
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
+
 fn star_one(input: &str) -> i64 {
     let mut sum = 0;
     for line in input.lines() {
@@ -10,25 +11,53 @@ fn star_one(input: &str) -> i64 {
             .map(|w| w.parse::<i64>())
             .filter_map(Result::ok)
             .collect::<Vec<i64>>();
-        let mut pyramid : Vec<Vec<i64>> = Vec::new();
-        pyramid.push(start_values);
-        while (!pyramid.last().unwrap().iter().all(|v|{*v==0})) {
-            let original_vec = pyramid.last().unwrap();
-            let differences: Vec<_> = original_vec
-                .iter()
-                .zip(original_vec.iter().skip(1))
-                .map(|(a, b)| b - a)
-                .collect();
-            pyramid.push(differences);
-        }
+        let pyramid = generatePyramids(start_values);
         sum += pyramid.iter().map(|inner_vec| *inner_vec.last().unwrap()).collect::<Vec<i64>>().iter().sum::<i64>();
-        println!("{:?}", pyramid);
+    }
+    return sum;
+}
+
+fn generatePyramids(start_values: Vec<i64>) -> Vec<Vec<i64>> {
+    let mut pyramid: Vec<Vec<i64>> = Vec::new();
+    pyramid.push(start_values);
+    while (!pyramid.last().unwrap().iter().all(|v| { *v == 0 })) {
+        let original_vec = pyramid.last().unwrap();
+        let differences: Vec<_> = original_vec
+            .iter()
+            .zip(original_vec.iter().skip(1))
+            .map(|(a, b)| b - a)
+            .collect();
+        pyramid.push(differences);
+    }
+    pyramid
+}
+
+fn star_two(input: &str) -> i64 {
+    let mut sum = 0;
+    for line in input.lines() {
+        let start_values = line
+            .split_ascii_whitespace()
+            .map(|w| w.parse::<i64>())
+            .filter_map(Result::ok)
+            .collect::<Vec<i64>>();
+        let pyramid = generatePyramids(start_values);
+        sum += pyramid.iter()
+            .map(|inner_vec| *inner_vec.first().unwrap())
+            .collect::<Vec<i64>>()
+            .iter()
+            .enumerate()
+            .map(|(index, &value)| if index % 2 == 1 { -value } else { value })
+            .collect::<Vec<i64>>()
+            .iter().sum::<i64>();
     }
     return sum;
 }
 
 
 fn main() {
-    println!("Example: {}", star_one(EXAMPLE));
-    println!("Input: {}", star_one(INPUT));
+    println!("Example: star1 {}", star_one(EXAMPLE));
+    println!("Input: star1 {}", star_one(INPUT));
+
+    println!("Example: star2 {}", star_two(EXAMPLE));
+    println!("Input: star2 {}", star_two(INPUT));
 }
