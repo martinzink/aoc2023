7104accd3abb14dc5e682bdee8c10f726a2b219e
Martin Zink <martinzink@apache.org>
2023-12-18 08:39:09 +0100

    polygon res

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,59 +1,69 @@
 use std::cmp;
-use geo::{Contains, Area, Coord, LineString, Polygon};
+use geo::{Contains, Area, Coord, LineString, Polygon, EuclideanLength};
 
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 struct InputLine {
-    dir_vec: Coord<i32>,
-    length: i32,
+    dir_vec: Coord<i64>,
+    length: i64,
     color_code: String
 }
 
 impl InputLine {
-    fn new(line: &str) -> Self {
-        let mut splits = line.split_ascii_whitespace();
-        let dir = splits.next().unwrap().chars().next().unwrap();
-        let dir_vec = match dir {
-            'U' => Coord{x:0, y:-1},
-            'R' => Coord{x:1, y:0},
-            'D' => Coord{x:0, y:1},
-            'L' => Coord{x:-1, y:0},
-            _ => {panic!("Invalid dir")}
-        };
-        let length = splits.next().unwrap().parse::<i32>().unwrap();
-        let color_code = splits.next().unwrap().to_string();
-        InputLine{dir_vec, length, color_code}
+    fn new(line: &str, part_two :bool) -> Self {
+        if !part_two {
+            let mut splits = line.split_ascii_whitespace();
+            let dir = splits.next().unwrap().chars().next().unwrap();
+            let dir_vec = match dir {
+                'U' => Coord{x:0, y:-1},
+                'R' => Coord{x:1, y:0},
+                'D' => Coord{x:0, y:1},
+                'L' => Coord{x:-1, y:0},
+                _ => {panic!("Invalid dir")}
+            };
+            let length = splits.next().unwrap().parse::<i64>().unwrap();
+            let mut color_code = splits.next().unwrap().to_string();
+            return InputLine{dir_vec, length, color_code};
+        } else {
+            let mut splits = line.split_ascii_whitespace();
+            let _ = splits.next().unwrap().chars().next().unwrap();
+            let _ = splits.next().unwrap().parse::<i64>().unwrap();
+            let mut color_code = splits.next().unwrap().to_string();
+            color_code.remove(8);
+            let dir_char = color_code.remove(7);
+            let dir_vec = match dir_char {
+                '3' => Coord{x:0, y:-1},
+                '0' => Coord{x:1, y:0},
+                '1' => Coord{x:0, y:1},
+                '2' => Coord{x:-1, y:0},
+                _ => {panic!("Invalid dir")}
+            };
+            color_code.remove(0);
+            color_code.remove(0);
+            let length = i64::from_str_radix(&*color_code, 16).unwrap();
+            return InputLine{dir_vec, length, color_code};
+        }
     }
 }
 
-fn get_edges(input: &str) -> i32{
-    let mut vertices: Vec<Coord<i32>> = Vec::new();
+fn get_edges(input: &str) -> i64{
+    let mut vertices: Vec<Coord<i64>> = Vec::new();
     vertices.push(Coord{x:0, y:0});
+    let mut perimeter = 0;
     for input_line_str in input.lines() {
-        let input_line = InputLine::new(input_line_str);
-        let next_vertex = *vertices.last().unwrap() + input_line.dir_vec*input_line.length;
+        let input_line = InputLine::new(input_line_str, true);
         vertices.push(*vertices.last().unwrap() + input_line.dir_vec*input_line.length);
-
-        //for i in 0..input_line.length {
-        //    vertices.push(*vertices.last().unwrap() + input_line.dir_vec);
-        //}
+        perimeter += input_line.length;
     }
-    let min_x = vertices.iter().min_by_key(|c|c.x).unwrap().x;
-    let max_x = vertices.iter().max_by_key(|c|c.x).unwrap().x;
-    let min_y = vertices.iter().min_by_key(|c|c.y).unwrap().y;
-    let max_y = vertices.iter().max_by_key(|c|c.y).unwrap().y;
-
-    let polygon = Polygon::new(LineString::new(vertices.clone()), vec![]);
-    let mut sum = 0;
-    for x in min_x..=max_x {
-        for y in min_y..=max_y {
-            let coord = Coord{x, y};
-            if polygon.contains(&coord) || polygon.exterior().contains(&coord) {
-                sum += 1;
-            }
-        }
-    }
-    return sum;
+    let mut shoe = 0;
+    // for vertex_window in vertices.windows(2) {
+    //     shoe += (vertex_window[0].x * vertex_window[1].y);
+    //     shoe -= (vertex_window[0].y * vertex_window[1].x);
+    // }
+    // return (shoe + perimeter)/2+1;
+    let vertices_f64 = vertices.iter().map(|c|{Coord{x:c.x as f64, y:c.y as f64}}).collect::<Vec<Coord<f64>>>();
+    let polygon = Polygon::new(LineString::new(vertices_f64.clone()), vec![]);
+    return (polygon.unsigned_area()+polygon.exterior().euclidean_length()/2.0 + 1.0) as i64;
 }
 
 fn main() {
